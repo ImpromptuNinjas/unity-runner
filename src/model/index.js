d7189cddac5ca35253a300ce6cce064d7eb84af6
Tyler Young <tyler.young@impromptu.ninja>
2020-08-02 20:23:39 -0400

    Update index.js

--- src/model/index.js
+++ src/model/index.js
@@ -2,6 +2,5 @@ import Action from './action';
 import Docker from './docker';
 import Input from './input';
 import ImageTag from './image-tag';
-import Output from './output';
 
-export { Action, Docker, Input, ImageTag, Output };
+export { Action, Docker, Input, ImageTag };
