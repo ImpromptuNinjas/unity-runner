fcb240406aa13ff5c5391c7883e2917458406335
Tyler Young <tyler.young@impromptu.ninja>
2020-08-02 20:22:26 -0400

    Update input.js

--- src/model/input.js
+++ src/model/input.js
@@ -2,9 +2,6 @@ import { getInput } from '@actions/core';
 import { includes } from 'lodash-es';
 
 class Input {
-  static get testModes() {
-    return ['all', 'playmode', 'editmode'];
-  }
 
   static isValidFolderName(folderName) {
     const validFolderName = new RegExp(/^(\.|\.\/)?(\.?\w+([-_]?\w+)*\/?)*$/);
@@ -15,17 +12,11 @@ class Input {
   static getFromUser() {
     // Input variables specified in workflow using "with" prop.
     const unityVersion = getInput('unityVersion') || '2019.2.11f1';
-    const testMode = getInput('testMode') || 'all';
     const rawProjectPath = getInput('projectPath') || '.';
-    const rawArtifactsPath = getInput('artifactsPath') || 'artifacts';
     const rawUseHostNetwork = getInput('useHostNetwork') || 'false';
-    const customParameters = getInput('customParameters') || '';
+    const args = getInput('args') || '';
 
     // Validate input
-    if (!includes(this.testModes, testMode)) {
-      throw new Error(`Invalid testMode ${testMode}`);
-    }
-
     if (!this.isValidFolderName(rawArtifactsPath)) {
       throw new Error(`Invalid artifactsPath "${rawArtifactsPath}"`);
     }
@@ -40,17 +31,14 @@ class Input {
 
     // Sanitise input
     const projectPath = rawProjectPath.replace(/\/$/, '');
-    const artifactsPath = rawArtifactsPath.replace(/\/$/, '');
     const useHostNetwork = rawUseHostNetwork === 'true';
 
     // Return sanitised input
     return {
       unityVersion,
       projectPath,
-      testMode,
-      artifactsPath,
       useHostNetwork,
-      customParameters,
+      args,
     };
   }
 }
